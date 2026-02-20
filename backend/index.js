866cae0df6d75c536d101a90823580a623f33cac
Arief Mohammad <amohammed2@miraclesoft.com>
2026-02-20 20:13:32 +0530

    second commit

--- backend/index.js
+++ backend/index.js
@@ -53,6 +53,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, "0.0.0.0", () => {
   console.log("Server running...");
 });
